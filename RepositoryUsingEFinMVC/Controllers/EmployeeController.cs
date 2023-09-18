2cf0cd01bb6cf685ad3bb9bc15c3a0938f7aee73
Evgeniy Gordienko <112853162+eudk@users.noreply.github.com>
2023-09-18 21:48:24 +0200

    Add project files.

---
+++ RepositoryUsingEFinMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,112 @@
+﻿using System.Web.Mvc;
+using RepositoryUsingEFinMVC.DAL;
+using RepositoryUsingEFinMVC.Repository;
+namespace RepositoryUsingEFinMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //Create a variable to hold the instance of EmployeeRepository
+        private IEmployeeRepository _employeeRepository;
+
+        //Initializing the _employeeRepository through parameterless constructor
+        public EmployeeController()
+        {
+            _employeeRepository = new EmployeeRepository(new EmployeeDBContext());
+        }
+
+        //If you want to Initialize _employeeRepository using Dependency Injection Container,
+        //Then include the following Parameterized Constructor
+        //public EmployeeController(IEmployeeRepository employeeRepository)
+        //{
+        //    _employeeRepository = employeeRepository;
+        //}
+
+        //The following Action Method is used to return all the Employees
+        [HttpGet]
+        public ActionResult Index()
+        {
+            //Call the GetAll Method of the EmployeeRepository instance
+            var model = _employeeRepository.GetAll();
+            return View(model);
+        }
+
+        //The following Action Method will open the Add Employee view
+        [HttpGet]
+        public ActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        //The following Action Method will be called when you click on the Submit button on the Add Employee view
+        [HttpPost]
+        public ActionResult AddEmployee(Employee model)
+        {
+            //First Check whether the Model State is Valid or not
+            if (ModelState.IsValid)
+            {
+                //If Model State is Valid, then call the Insert method EmployeeRepository to make the Entity State Added
+                _employeeRepository.Insert(model);
+                //To make the changes permanent in the database, call the Save method of EmployeeRepository
+                _employeeRepository.Save();
+                //Once the data is saved into the database, redirect to the Index View
+                return RedirectToAction("Index", "Employee");
+            }
+
+            //If the Model state is not valid, then stay on the current AddEmployee view
+            return View();
+        }
+
+        //The following Action Method will open the Edit Employee view based on the EmployeeId
+        [HttpGet]
+        public ActionResult EditEmployee(int EmployeeId)
+        {
+            //First, Fetch the Employee information by calling the GetById method of EmployeeRepository
+            Employee model = _employeeRepository.GetById(EmployeeId);
+            //Then Pass the Employee data to the Edit view
+            return View(model);
+        }
+
+        //The following Action Method will be called when you click on the Submit button on the Edit Employee view
+        [HttpPost]
+        public ActionResult EditEmployee(Employee model)
+        {
+            //First Check whether the Model State is Valid or not
+            if (ModelState.IsValid)
+            {
+                //If Valid, then call the Update Method of EmployeeRepository  to make the Entity State Modified
+                _employeeRepository.Update(model);
+                //To make the changes permanent in the database, call the Save method of EmployeeRepository
+                _employeeRepository.Save();
+                //Once the updated data is saved into the database, redirect to the Index View
+                return RedirectToAction("Index", "Employee");
+            }
+            else
+            {
+                //If the Model State is invalid, then stay on the same view
+                return View(model);
+            }
+        }
+
+        //The following Action Method will open the Delete Employee view based on the EmployeeId
+        [HttpGet]
+        public ActionResult DeleteEmployee(int EmployeeId)
+        {
+            //First, Fetch the Employee information by calling the GetById method of EmployeeRepository
+            Employee model = _employeeRepository.GetById(EmployeeId);
+            //Then Pass the Employee data to the Delete view
+            return View(model);
+        }
+
+        //The following Action Method will be called when you click on the Submit button on the Delete Employee view
+        [HttpPost]
+        public ActionResult Delete(int EmployeeID)
+        {
+            //Call the Delete Method of the EmployeeRepository to Make the Entity State Deleted 
+            _employeeRepository.Delete(EmployeeID);
+            //Then Call the Save Method to delete the entity from the database permanently
+            _employeeRepository.Save();
+            //And finally, redirect the user to the Index View
+            return RedirectToAction("Index", "Employee");
+        }
+    }
+}
